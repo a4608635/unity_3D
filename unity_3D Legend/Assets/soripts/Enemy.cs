d68d14840049cf7e0ae86eb021b07921af6eb21c
a4608635 <57396749+a4608635@users.noreply.github.com>
2020-01-14 21:29:18 +0800

    2020/1/14 角色追蹤

---
+++ unity_3D Legend/Assets/soripts/Enemy.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using UnityEngine.AI;       // 引用 人工智慧 API
+
+// 內容收起 ctrl+m+o
+// 內容展開 ctrl+m+l
+
+public class Enemy : MonoBehaviour
+{
+    [Header("怪物資料")]
+    public EnemyData data;
+
+    private Animator ani;          // 動畫控制器       
+    private NavMeshAgent nav;       // 導覽網隔代理器
+
+    private Transform tra;          // 玩家變形
+
+    private void Start()
+    {
+        ani = GetComponent<Animator>();        
+        nav = GetComponent<NavMeshAgent>();     
+        nav.speed = data.speed;                 // 調整 代理器.速度
+        nav.stoppingDistance = data.stopDistance;
+
+        tra = GameObject.Find("Fox").GetComponent<Transform>();     // 取得玩家變形
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    /// <summary>
+    /// 等待
+    /// </summary>
+    private void Wait()
+    {
+
+    }
+
+    /// <summary>
+    /// 移動
+    /// </summary>
+    private void Move()
+    {
+        Vector3 posTra = tra.position;          // 區域三維向量 = 目標.座標
+        posTra.y = transform.position.y;        // 三維向量.y = 本身.y
+        transform.LookAt(posTra);               // 變形.看著(三維向量)
+
+        ani.SetBool("跑步開關", true);
+
+        nav.SetDestination(tra.position);       // 代理器.設定目的地(玩家
+    }
+
+    /// <summary>
+    /// 攻擊
+    /// </summary>
+    private void Attack()
+    {
+
+    }
+
+    /// <summary>
+    /// 受傷
+    /// </summary>
+    /// <param name="damage">接收玩家給予的傷害直</param>
+    private void Hit(float damage)
+    {
+
+    }
+
+    /// <summary>
+    /// 死亡
+    /// </summary>
+    private void Dead()
+    {
+
+    }
+}
---
+++ unity_3D Legend/Assets/soripts/EnemyData.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+// ScriptableObject 腳本化物件:將腳本的資存放在專案內(不需要掛在物件上)
+
+[CreateAssetMenu(fileName = "怪物資料", menuName = "Agfeuft/怪物資料")]
+public class EnemyData : ScriptableObject
+{
+    [Header("移動速度"),Range(0,100)]
+    public float speed;
+    [Header("生命"), Range(100, 10000)]
+    public float hp;
+    [Header("攻擊"), Range(1, 100)]
+    public float attack;
+    [Header("冷卻時間"), Range(1, 10)]
+    public float cd;
+    [Header("停止距離"), Range(0.1F, 100)]
+    public float stopDistance;
+}
