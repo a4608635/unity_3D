42905baa2e94fd0b2aa439db0e794b6a0949a811
a4608635 <57396749+a4608635@users.noreply.github.com>
2020-02-25 21:19:53 +0800

    2020/2/25 結束啦

---
+++ unity_3D Legend/Assets/soripts/MenuManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    [Header("玩家資料")]
+    public PleyerData dataPlayer;
+
+    public void BuyHp_500()
+    {
+        dataPlayer.hpmax += 500;                // 生命最大值遞增
+        dataPlayer.hp = dataPlayer.hpmax;       // 目前生命=生命最大值
+    }
+
+    public void BuyAtk_50()
+    {
+        dataPlayer.attack += 50;                // 攻擊力遞增
+    }
+
+    public void LoadLeve1()
+    {
+        dataPlayer.hp = dataPlayer.hpmax;
+        SceneManager.LoadScene("關卡1");
+    }
+}
